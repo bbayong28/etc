67e5429ace84ac882927abe1156cb4b916c46913
apple <ghkdud5795@hanmail.net>
2023-03-02 02:44:10 +0900

    20230302

--- js/momentum/js/main.js
+++ js/momentum/js/main.js
@@ -1,4 +1,4 @@
-const images = ["0.jpg", "1.jpg", "2.png", "3.png", "4.jpg", "5.jpg","6.png","7.png","8.jpg","9.jpg","10.jpg","11.jpg","12.jpg","13.jpg","14.jpg","15.jpg","16.jpg","17.jpg","18.jpg","19.jpg","20.jpg","21.jpg","22.jpg","23.jpg","24.jpg","25.jpg","26.jpg","27.jpg","28.jpg","29.jpg","30.jpg","31.jpg"];
+const images = ["0.jpg", "1.jpg", "2.png", "3.png", "4.jpg", "5.jpg", "6.png", "7.png", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", "16.jpg", "17.jpg", "18.jpg", "19.jpg", "20.jpg", "21.jpg", "22.jpg", "23.jpg", "24.jpg", "25.jpg", "26.jpg", "27.jpg", "28.jpg", "29.jpg", "30.jpg","31.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
